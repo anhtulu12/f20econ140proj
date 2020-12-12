7fa471e5d998a1853582ce620ea29f817fe93691
renasong <renasong@berkeley.edu>
2020-12-11 16:53:08 -0800

    Update script.R

--- R/script.R
+++ R/script.R
@@ -7,21 +7,13 @@ library(stargazer)
 #Replace path depending on user
 data <- read_csv("/Users/renasong/Documents/github/f20econ140proj/data_cleaning/data_clean.csv")
 
-#Draw scatterplots for EDA
+#Draw scatterplots for EDA: no linear relationships
 
 ggplot(data=data,aes(x=testscore,y=houseprice))+
   geom_point()+
   xlab('Mean Test Score')+
   ylab('Median House Price')+
   ggtitle('California Test Scores (CAASPP) v. Median House Price')
-
-
-data$recip.ces <- 1/data$ces
-ggplot(data=data,aes(x=recip.ces,y=houseprice))+
-  geom_point()+
-  xlab('Reciprocal of CES Score')+
-  ylab('Median House Price')+
-  ggtitle('Pollution (CES) v. Median House Price')
 
 ggplot(data=data,aes(x=ces,y=houseprice))+
   geom_point()+
@@ -33,6 +25,26 @@ ggplot(data=data,aes(x=transitscore,y=houseprice))+
   geom_point()+
   xlab('Transit Performance Score')+
   ylab('Median House Price')+
+  ggtitle('Transit Quality v. Median House Price')
+
+#plots of nonlinear relationships
+
+ggplot(data=data,aes(x=testscore,y=log(houseprice)))+
+  geom_point()+
+  xlab('Mean Test Score')+
+  ylab('log of Median House Price')+
+  ggtitle('California Test Scores (CAASPP) v. Median House Price')
+
+ggplot(data=data,aes(x=ces,y=log(houseprice)))+
+  geom_point()+
+  xlab('CES Score')+
+  ylab('log of Median House Price')+
+  ggtitle('Pollution (CES) v. Median House Price')
+
+ggplot(data=data,aes(x=transitscore,y=log(houseprice)))+
+  geom_point()+
+  xlab('Transit Performance Score')+
+  ylab('log of Median House Price')+
   ggtitle('Transit Quality v. Median House Price')
 
 
@@ -48,21 +60,18 @@ summary(reg2)
 reg3 <-  lm(log(data$houseprice) ~ data$ces, data=data)
 summary(reg3)
 
-#Regression 4: log(houseprice) on 1/ces
-reg4 <-  lm(log(data$houseprice) ~ data$recip.ces, data=data)
+
+#Regression 4: log(houseprice) on test score, transit score, ces
+reg4 <-  lm(log(data$houseprice) ~ data$testscore + data$transitscore + 
+              data$ces, data=data)
 summary(reg4)
 
-#Regression 5: log(houseprice) on test score, transit score, ces, 1/ces
-reg5 <-  lm(log(data$houseprice) ~ data$testscore + data$transitscore + 
-              data$ces + data$recip.ces, data=data)
-summary(reg5)
 
 
 
 
-
-stargazer(reg1, reg2, reg3, reg4, reg5,
-          covariate.labels = c("Test Score", "Transit Score", "CES", "1/CES"),
+stargazer(reg1, reg2, reg3, reg4,
+          covariate.labels = c("Test Score", "Transit Score", "CES"),
           out='results.doc',
           type='html',
           title='Results',
